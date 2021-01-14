6e1a16dc87cc6a2da3037261516528d9da71f8e3
RiveSunder <rivesunder@protonmail.com>
2021-01-14 15:40:04 -0700

    add random agent

--- carle/agents.py
+++ carle/agents.py
@@ -12,6 +12,36 @@ from mcl import RND2D
 import matplotlib.pyplot as plt
 
 
+class RandomAgent(nn.Module):
+
+    def __init__(self, **kwargs):
+        super(RandomAgent, self).__init__()
+
+        self.action_width = kwargs["action_width"] \
+                if "action_width" in kwargs.keys()\
+                else 32
+        self.action_height = kwargs["action_height"] \
+                if "action_height" in kwargs.keys()\
+                else 32
+        self.observation_width = kwargs["observation_width"] \
+                if "action_width" in kwargs.keys()\
+                else 64
+        self.observation_height = kwargs["observation_height"] \
+                if "action_height" in kwargs.keys()\
+                else 64
+
+        self.toggle_rate = 0.020
+
+    def forward(self, obs):
+
+        instances = obs.shape[0]
+        action = 1.0 \
+            * (torch.rand(instances,1,self.action_width, self.action_height)\
+                <= self.toggle_rate)
+
+        return action
+
+
 class RandomNetworkAgent(nn.Module):
 
     def __init__(self, **kwargs):
@@ -32,6 +62,7 @@ class RandomNetworkAgent(nn.Module):
 
         self.depth = 3
         self.filter_dim = 4
+        self.toggle_rate = 0.1
 
 
 
@@ -63,7 +94,7 @@ class RandomNetworkAgent(nn.Module):
 
         output = self.network(obs)
 
-        action = 1.0 * (output <= torch.rand_like(output))
+        action = 1.0 * (output <= self.toggle_rate)
 
         action = action.reshape(instances, 1, \
                 self.action_width, self.action_height)
@@ -78,49 +109,50 @@ if __name__ == "__main__":
     #mouse_maze: 12345/37
     #walled_cities: 2345/45678
 
+    if(0):
+        my_steps = 512
+
+        for rules, name in zip([[[2,3],[3]], [[1,2,3,4,5],[3,7]]],\
+                ["life", "mouse_maze"]):
+            env_fn = AutomaticCellularEnvironment 
+            env = RND2D(env_fn)
+
+            env.env.survive = rules[0]
+            env.env.birth = rules[1]
+            env.env.instances = 1
+            env.env.batch_size = 32
+
+
+            action = torch.ones(env.env.instances,1,32,32)
+            obs = env.reset()
+            rewards = []
+            steps = []
+            for step in range(my_steps):
+
+                obs, reward, done, info = env.step(action)
+
+                rewards.append(reward.squeeze().detach().numpy())
+                steps.append(step)
+
+                #env.env.save_frame()
+
+                fig = plt.figure(figsize=(6,12))
+                plt.subplot(211)
+                plt.plot(steps, rewards)
+                plt.plot(steps[-1], rewards[-1], "o")
+                plt.title("ones policy, RND reward in {} CA".format(name))
+                plt.subplot(212)
+                plt.imshow(obs[0,0,:,:].detach().numpy())
+                plt.title("{} CA".format(name))
+                plt.savefig("./frames/ones_rewards_{}_step{}".format(name, step))
+                plt.close(fig)
+
     my_steps = 1024
 
+    action = torch.ones(1,1,32,32)
     for rules, name in zip([[[2,3],[3]], [[1,2,3,4,5],[3,7]]],\
             ["life", "mouse_maze"]):
-        agent = RandomNetworkAgent() 
-        env_fn = AutomaticCellularEnvironment 
-        env = RND2D(env_fn)
-
-        env.env.survive = rules[0]
-        env.env.birth = rules[1]
-        env.env.instances = 1
-        env.env.batch_size = 32
-
-
-        action = torch.ones(env.env.instances,1,32,32)
-        obs = env.reset()
-        rewards = []
-        steps = []
-        for step in range(my_steps):
-
-            obs, reward, done, info = env.step(action)
-
-            rewards.append(reward.squeeze().detach().numpy())
-            steps.append(step)
-
-            #env.env.save_frame()
-
-            fig = plt.figure(figsize=(6,12))
-            plt.subplot(211)
-            plt.plot(steps, rewards)
-            plt.plot(steps[-1], rewards[-1], "o")
-            plt.title("ones policy, RND reward in {} CA".format(name))
-            plt.subplot(212)
-            plt.imshow(obs[0,0,:,:].detach().numpy())
-            plt.title("{} CA".format(name))
-            plt.savefig("./frames/ones_rewards_{}_step{}".format(name, step))
-            plt.close(fig)
-
-    my_steps = 8192
-
-    for rules, name in zip([[[2,3],[3]], [[1,2,3,4,5],[3,7]]],\
-            ["life", "mouse_maze"]):
-        agent = RandomNetworkAgent() 
+        agent = RandomAgent() 
         env_fn = AutomaticCellularEnvironment 
         env = RND2D(env_fn)
 
@@ -133,7 +165,7 @@ if __name__ == "__main__":
         obs = env.reset()
         rewards = []
         steps = []
-
+        print("toggle rate: ", action.mean())
         for step in range(my_steps):
 
             action = agent(obs)
