dcfc67fe53b91052922f0cfb7655f2b8bab3fcfd
riveSunder <38316715+riveSunder@users.noreply.github.com>
2021-04-17 11:42:49 -0600

    remove duck
    as per comments from myself

--- test/test_all.py
+++ test/test_all.py
@@ -4,13 +4,7 @@ import argparse
 from test.test_reset import TestReset
 
 if __name__ == "__main__":
-    """
-        __
-      <(o )__
-       (._> /
 
-        DUCK
-    """
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-v", "--verbosity", default=0, \
