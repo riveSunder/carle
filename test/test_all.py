f83beb112ce7440bbc0dec341b4c09952ba6b92f
RiveSunder <RiveSunder@protonmail.com>
2021-04-17 11:40:00 -0600

    add a duck

--- test/test_all.py
+++ test/test_all.py
@@ -4,6 +4,13 @@ import argparse
 from test.test_reset import TestReset
 
 if __name__ == "__main__":
+    """
+        __
+      <(o )__
+       (._> /
+
+        DUCK
+    """
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-v", "--verbosity", default=0, \
