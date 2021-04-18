f1a315187ca585736a13aed560ef3b08a823bac7
RiveSunder <RiveSunder@protonmail.com>
2021-04-18 13:41:17 -0600

    try for pip install from requirements.txt to work

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="carle",\
-        py_modules=["carle", "test", "evaluation"],\
+        py_modules=["my_carle", "test", "evaluation"],\
         install_requires=["numpy==1.18.4",\
                         "torch==1.5.1",\
                         "scikit-image==0.17.2",\
